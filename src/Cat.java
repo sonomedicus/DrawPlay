f16bbe69284d9e21b641b60ce034680a6ded3efb
Trevor Worth <tworth@mines.edu>
2015-02-02 12:20:24 -0700

    Changed the color of the head, eyes, and mouth

--- src/Cat.java
+++ src/Cat.java
@@ -30,17 +30,18 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		// The cat now has a magenta head, black eyes, and a blue mouth
+		g2.setColor(Color.magenta);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.black);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.blue);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
